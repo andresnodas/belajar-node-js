3b9beb4ff83f30b8fc05630f63ce3199b0ed525e
Andres Nodas <nodasiniesta@gmail.com>
2022-08-20 10:21:23 +0700

    Moved from repository 9---Dynamic-Routes-and-Advanced-Models

---
+++ 9-Dynamic-Routes-and-Advanced-Models/models/product.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+const path = require('path');
+
+const Cart = require('./cart');
+
+const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
+
+const getProductsFromFile = callback => {
+    fs.readFile(p, (err, fileContent) => {
+        if(err) {
+            return callback([]);
+        }
+        
+        return callback(JSON.parse(fileContent));
+    });
+}
+
+module.exports = class Product {
+    constructor(id, title, imageUrl, description, price){
+        this.id = id;
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
+    }
+
+    save() {
+        getProductsFromFile(products => {
+            if(this.id){
+                const existingProductIndex = products.findIndex(object => object.id === this.id);
+                const updatedProducts = [...products];
+                updatedProducts[existingProductIndex] = this;
+
+                fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+                    console.log(err);
+                });
+            } else {
+                this.id = Math.random().toString();
+
+                products.push(this);
+            
+                fs.writeFile(p, JSON.stringify(products), (err) => {
+                    console.log(err);
+                });
+            }
+        });
+    }
+
+    static deleteById(id){
+        getProductsFromFile(products => {
+            const product = products.find(object => object.id === id);
+            const updatedProducts = products.filter(object => object.id !== id);
+            fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+                if(!err){
+                    Cart.deleteProduct(id, product.price);
+                }
+            });
+        });
+    }
+
+    static fetchAll(callback) {
+        getProductsFromFile(callback);
+    }
+
+    static findById(id, callback) {
+        getProductsFromFile(products => {
+            const product = products.find(object => object.id === id);
+            callback(product);
+        });
+    }
+}
---
+++ 9-Dynamic-Routes-and-Advanced-Models/models/product3.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
+
+const getProductsFromFile = callback => {
+    fs.readFile(p, (err, fileContent) => {
+        if(err) {
+            return callback([]);
+        }
+        
+        return callback(JSON.parse(fileContent));
+    });
+}
+
+module.exports = class Product {
+    constructor(title, imageUrl, description, price){
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
+    }
+
+    save() {
+        this.id = Math.random().toString();
+        getProductsFromFile(products => {
+            products.push(this);
+            
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
+    }
+
+    static fetchAll(callback) {
+        getProductsFromFile(callback);
+    }
+
+    static findById(id, callback) {
+        getProductsFromFile(products => {
+            const product = products.find(object => object.id === id);
+            callback(product);
+        });
+    }
+}
